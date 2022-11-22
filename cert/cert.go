002a79b599dc39dc012943ca2ce9ef2946aa43a6
Dylan Germann <dev.dyger@gmail.com>
2022-11-22 10:28:51 +0100

    feat: format the date

--- cert/cert.go
+++ cert/cert.go
@@ -34,16 +34,20 @@ func New(course, name, date string) (*Cert, error) {
 	if err != nil {
 		return nil, err
 	}
-	d := date
+	d, err := parseDate(date)
+	if err != nil {
+		return nil, err
+	}
 
 	cert := &Cert{
 		Course:             c,
 		Name:               n,
+		Date:               d,
 		LabelTitle:         fmt.Sprintf("%v Certificate - %v", c, n),
 		LabelCompletion:    "Certificate of Completion",
 		LabelPresented:     "This Certificate is presented To",
 		LabelParticipation: fmt.Sprintf("For participation in the %v", c),
-		LabelDate:          fmt.Sprint("Date: ", d),
+		LabelDate:          fmt.Sprint("Date: ", d.Format("02/01/2006")),
 	}
 	return cert, nil
 }
@@ -75,4 +79,11 @@ func validateStr(str string, maxLen int) (string, error) {
 
 	}
 	return c, nil
+}
+func parseDate(date string) (time.Time, error) {
+	t, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return t, err
+	}
+	return t, nil
 }
