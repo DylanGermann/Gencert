98162b8d441e006d50e5e70741644ef1520ad72f
Dylan Germann <dev.dyger@gmail.com>
2022-11-25 13:58:16 +0100

    feat: add the footer

--- pdf/certpdf.go
+++ pdf/certpdf.go
@@ -34,8 +34,15 @@ func (p *PdfSaver) Save(cert cert.Cert) error {
 	// Background
 	background(pdf)
 
-	//Header
+	// Header
 	header(pdf, &cert)
+	pdf.Ln(30)
+
+	// Body
+	body(pdf, &cert)
+
+	// Footer
+	footer(pdf)
 
 	//save file
 	filename := fmt.Sprintf("%v.pdf", cert.LabelTitle)
@@ -46,6 +53,40 @@ func (p *PdfSaver) Save(cert cert.Cert) error {
 	}
 	fmt.Printf("Saved certificate to '%v'\n", path)
 	return nil
+}
+
+func footer(pdf *gofpdf.Fpdf) {
+	opts := gofpdf.ImageOptions{
+		ImageType: "png",
+	}
+	imageWidth := 50.0
+	filename := "img/stamp.png"
+	pageWidth, pageHeight := pdf.GetPageSize()
+	x := pageWidth - imageWidth - 20.0
+	y := pageHeight - imageWidth - 10.0
+	pdf.ImageOptions(filename, x, y, imageWidth, 0, false, opts, 0, "")
+}
+
+func body(pdf *gofpdf.Fpdf, c *cert.Cert) {
+	// Body
+	pdf.SetFont("Helvetica", "I", 20)
+	pdf.WriteAligned(0, 50, c.LabelPresented, "C")
+	pdf.Ln(30)
+
+	// Body - Student name
+	pdf.SetFont("Times", "B", 40)
+	pdf.WriteAligned(0, 50, c.Name, "C")
+	pdf.Ln(30)
+
+	// Body - Participation
+	pdf.SetFont("Helvetica", "I", 20)
+	pdf.WriteAligned(0, 50, c.LabelParticipation, "C")
+	pdf.Ln(30)
+
+	// Body - Date
+	pdf.SetFont("Helvetica", "I", 15)
+	pdf.WriteAligned(0, 50, c.LabelDate, "C")
+	pdf.Ln(30)
 }
 
 func header(pdf *gofpdf.Fpdf, c *cert.Cert) {
